c4e5d4d7077c83015833863df345760eac19360f
Silvio Fernando <silvio-fernando2009@hotmail.com>
2019-10-09 13:19:38 -0300

    start

---
+++ Circulo/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Circulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pcb = new System.Windows.Forms.PictureBox();
+            this.Btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt4 = new System.Windows.Forms.TextBox();
+            this.Txt3 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Pcb)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pcb
+            // 
+            this.Pcb.Location = new System.Drawing.Point(209, 12);
+            this.Pcb.Name = "Pcb";
+            this.Pcb.Size = new System.Drawing.Size(579, 426);
+            this.Pcb.TabIndex = 0;
+            this.Pcb.TabStop = false;
+            // 
+            // Btn
+            // 
+            this.Btn.Location = new System.Drawing.Point(12, 387);
+            this.Btn.Name = "Btn";
+            this.Btn.Size = new System.Drawing.Size(191, 51);
+            this.Btn.TabIndex = 2;
+            this.Btn.Text = "Create";
+            this.Btn.UseVisualStyleBackColor = true;
+            this.Btn.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Result);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.Txt4);
+            this.groupBox1.Controls.Add(this.Txt3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Txt2);
+            this.groupBox1.Controls.Add(this.Txt1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 369);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X:";
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(130, 39);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(55, 20);
+            this.Txt2.TabIndex = 1;
+            this.Txt2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt2_KeyPress);
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(26, 39);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(55, 20);
+            this.Txt1.TabIndex = 0;
+            this.Txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt1_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ponto Inicial";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Segundo Ponto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(108, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 14);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "X:";
+            // 
+            // Txt4
+            // 
+            this.Txt4.Location = new System.Drawing.Point(130, 104);
+            this.Txt4.Name = "Txt4";
+            this.Txt4.Size = new System.Drawing.Size(55, 20);
+            this.Txt4.TabIndex = 7;
+            // 
+            // Txt3
+            // 
+            this.Txt3.Location = new System.Drawing.Point(26, 104);
+            this.Txt3.Name = "Txt3";
+            this.Txt3.Size = new System.Drawing.Size(55, 20);
+            this.Txt3.TabIndex = 6;
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(84, 204);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(0, 13);
+            this.Result.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Btn);
+            this.Controls.Add(this.Pcb);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Pcb)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pcb;
+        private System.Windows.Forms.Button Btn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Txt4;
+        private System.Windows.Forms.TextBox Txt3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Result;
+    }
+}
+
---
+++ Circulo/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Text;
+
+namespace Circulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        
+        private void Draw()
+        {
+            Graphics graphics = Pcb.CreateGraphics();
+            Pcb.Refresh();
+            Point ponto1 = new Point(Convert.ToInt32(Txt1.Text), Convert.ToInt32(Txt2.Text));
+            float raio = 50;
+            Point ponto2 = new Point(Convert.ToInt32(Txt3.Text), Convert.ToInt32(Txt4.Text) + (int)raio);
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+
+            for (int x = 0; x < 1000; ++x)
+            {
+
+                for (int y = 0; y < 1000; ++y)
+                {
+                    Point PontoAleatorio = new Point(x, y);
+                    if (EquacaoCirco(ponto1, PontoAleatorio) == raio)
+                    {
+                        graphics.FillEllipse(new SolidBrush(Color.Black), PontoAleatorio.X, PontoAleatorio.Y, 6, 6);
+
+                    }
+
+                }
+
+            };
+
+
+            graphics.FillEllipse(new SolidBrush(Color.Red), ponto1.X, ponto1.Y, 6, 6);
+
+            graphics.FillEllipse(new SolidBrush(Color.Red), ponto2.X, ponto2.Y, 6, 6);
+            graphics.DrawLine(new Pen(Color.Red), new Point(ponto1.X+3, ponto1.Y) , new Point(ponto2.X+3, ponto2.Y));
+
+            if ((EquacaoCirco(ponto1, ponto2) == raio))
+            {
+                Result.Text = "Ponto está dentro da circunferência";
+            }else if ((EquacaoCirco(ponto1, ponto2) > raio))
+            {
+                Result.Text = "Ponto está fora da circunferência";
+            }else
+            {
+                Result.Text = "Ponto está dentro da circunferência";
+            }
+        }
+
+        public int EquacaoCirco(Point Inicial, Point PontoAleatorio)
+        {
+
+            
+            return (int)Math.Sqrt(Math.Pow((PontoAleatorio.X - Inicial.X), 2) + Math.Pow((PontoAleatorio.Y - Inicial.Y), 2));
+            //((int)raiovalor == (int)raio);
+        }
+
+
+        private void Txt2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Btn_Click(sender, e);
+            }
+        }
+
+        private void Txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Btn_Click(sender, e);
+            }
+        }
+    }
+}
+
